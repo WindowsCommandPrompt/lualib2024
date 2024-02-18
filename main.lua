d43c2fbbf2550fa0e071b4614c76672ac892333e
Li Zhe Yun <93893933+WindowsCommandPrompt@users.noreply.github.com>
2024-02-19 00:15:01 +0800

    Update main.lua
    scaffold

--- main.lua
+++ main.lua
@@ -1,4 +1,20 @@
  local moduleCore = require("luafunctions")
  
- local arr = moduleCore.Array()
+ local arrInit = moduleCore.Array()
  local arrStatic = moduleCore._("Array")
+ 
+ local FILE = io.open("main.luapp", "r")
+ 
+ function main(source)
+     print(source)
+ end
+ 
+ --Method call
+  if FILE then 
+     local content = FILE:read("*a")
+     main(content)
+     FILE:close()
+  else 
+      error("No such file found. Compilation failed.")
+  end
+ --End
